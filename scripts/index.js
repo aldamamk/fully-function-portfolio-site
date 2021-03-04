03f6dfa115172bd651878b7ec99d71438a05446a
krizzel <72677658+aldamamk@users.noreply.github.com>
2021-03-03 18:29:42 -0800

    changes to portfolio

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,5 @@
-// ============ accordion 
+// ============ always start load on top
 
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-    panel.style.display = "none";
-    } else {
-    panel.style.display = "block";
-    }
-});
-}
-
+$(window).on('beforeunload', function(){
+    $(window).scrollTop(0);
+  });
